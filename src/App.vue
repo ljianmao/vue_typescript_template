31b9ab282a341e1dabeb8cb91f882c1072982f8c
ljianmao <ljianmao@gmail.com>
2019-08-09 10:33:14 +0800

    vue webpack 的typescript模板初始化

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        <img src="./assets/logo.png" />
+        <router-view />
+    </div>
+</template>
+
+<script loang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    name: 'App'
+})
+// export default {
+//     name: 'App'
+// }
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
